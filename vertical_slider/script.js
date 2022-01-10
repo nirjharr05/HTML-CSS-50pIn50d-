ebab967cc8eb306cbdf468f433ac85bcae0b6ac5
Nirjhar Roy <56313591+nirjharr05@users.noreply.github.com>
2022-01-10 15:24:18 +0530

    mac upload

---
+++ vertical_slider/script.js
@@ -0,0 +1,34 @@
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activesSlideIndex = 0;
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activesSlideIndex++;
+    if (activesSlideIndex > slidesLength - 1) {
+      activesSlideIndex = 0;
+    }
+  } else if (direction === "down") {
+    activesSlideIndex--;
+    if (activesSlideIndex < 0) {
+      activesSlideIndex = slidesLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activesSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activesSlideIndex * sliderHeight
+  }px)`;
+};
